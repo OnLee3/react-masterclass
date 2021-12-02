b70847d72fdde003312ecc4d35db1f55ed84aafa
LEON <dlwotjr2@gmail.com>
2021-12-02 23:20:44 +0900

    Definitely Typed Setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 const Title = styled.h1`
-  color: ${(props) => props.theme.textColor}
+  color: ${(props) => props.theme.textColor};
 `;
 
 const Wrapper = styled.div`
-  background-color: ${(props) => props.theme.backgroundColor};
   display: flex;
-  width: 100vw;
   height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
-  <Wrapper>
-    <Title>Hello</Title>
-  </Wrapper>
-  )
+    <Wrapper>
+      <Title>Hello</Title>
+    </Wrapper>
+  );
 }
 
 export default App;
