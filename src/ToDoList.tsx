d572b1ca13f84ccdfea8b6fece1fa38864a25c90
LEON <dlwotjr2@gmail.com>
2022-02-01 18:34:46 +0900

    5.8 required, error 메세지 추가

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,27 +1,80 @@
-import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
-function ToDoList(){
-    const { register, handleSubmit, formState } = useForm();
-    const onValid = (data:any) => {
-        console.log(data)
-    }
-    console.log(formState.errors)
-    const inValid = (data:any) => {
-        console.log(data)
-    }
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  password1: string;
+}
 
-    return <div>
-        <form style={{display: "flex", flexDirection: "column"}} onSubmit={handleSubmit(onValid, inValid)}>
-            <input {...register("email", { required: true })} placeholder="Email"/>
-            <input {...register("firstName", { required: true } )} placeholder="First Name"/>
-            <input {...register("lastName", { required: true } )} placeholder="Last Name"/>
-            <input {...register("username", { required: true, minLength: 10 } )} placeholder="Username"/>
-            <input {...register("password", { required: true, minLength: 5 } )} placeholder="Password"/>
-            <input {...register("password1", { required: "Password is required.", minLength: { value:5, message: "Your password is too short." } } )} placeholder="Password1"/>
-            <button>Add</button>
-        </form>
-    </div>;
+function ToDoList() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({
+    defaultValues: {
+      email: "@naver.com",
+    },
+  });
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  const inValid = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid, inValid)}
+      >
+        <input
+          {...register("email", {
+            required: "Email is required.",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.com emails allowed",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.email?.message}</span>
+        <input
+          {...register("firstName", { required: "Write here." })}
+          placeholder="First Name"
+        />
+        <span>{errors?.firstName?.message}</span>
+        <input
+          {...register("lastName", { required: "Write here." })}
+          placeholder="Last Name"
+        />
+        <span>{errors?.lastName?.message}</span>
+        <input
+          {...register("username", { required: "Write here.", minLength: 10 })}
+          placeholder="Username"
+        />
+        <span>{errors?.username?.message}</span>
+        <input
+          {...register("password", { required: "Write here.", minLength: 5 })}
+          placeholder="Password"
+        />
+        <span>{errors?.password?.message}</span>
+        <input
+          {...register("password1", {
+            required: "Password is required.",
+            minLength: { value: 5, message: "Your password is too short." },
+          })}
+          placeholder="Password1"
+        />
+        <span>{errors?.password1?.message}</span>
+        <button>Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default ToDoList;
